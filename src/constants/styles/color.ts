3e758ad5b64f17db235fe7e02a8b3299bd02beb1
hoyy <110888511+hoyyChoi@users.noreply.github.com>
2023-05-08 17:40:17 +0900

    Feat/navbar (#2)
    * [fix]라이브러리 삭제 및 생성
    
    * [add] 나눔스퀘어네오 font 추가
    
    * [add] 컬러 변수 추가
    
    * [feat] 네비게이션바 작업
    
    * [fix] fix lint settings and modify folder names
    
    ---------
    
    Co-authored-by: ballsona <ballsona0925@naver.com>

---
+++ src/constants/styles/color.ts
@@ -0,0 +1,23 @@
+
+
+export const COLORS = {
+    primary: {
+      purple: '#5C59F0',
+    },
+    grayscale: {
+      white: '#FFFFFF',
+      gray50: '#ADADAD',
+      gray100: '#9E9E9E',
+      gray200: '#818181',
+      gray300: '#636363',
+      gray400: '#464646',
+      gray500: '#282828',
+      gray600: '#202020',
+      gray700: '#181818',
+      gray800: '#101010',
+      gray900: '#080808',
+    },
+
+  };
+
+
