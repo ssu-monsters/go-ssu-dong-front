251761639f44fb9bc4236517b97205223b177286
hoyyChoi <hoyeon8165@gmail.com>
2023-05-09 19:23:25 +0900

    [feat] 네비게이션 바 작업

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,52 +1,111 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { useRouter } from "next/router";
 export interface NavBarProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 const Navbar = ({ children }: NavBarProps) => {
-  const [btn, setBtn] = useState('홍보글')
+  const [btn, setBtn] = useState("");
+  const [drop, setDrop] = useState(false);
+  const [auth, setAuth] = useState(false);
+
+  const router = useRouter();
 
   const select = (item: any) => {
-    if (item === '홍보글') {
-      setBtn('홍보글')
-    } else if (item === '지원') {
-      setBtn('지원')
-    } else if (item === '모집') {
-      setBtn('모집')
+    if (item === "홍보글") {
+      setBtn("홍보글");
+      router.push("/promotion");
+    } else if (item === "지원") {
+      setBtn("지원");
+      router.push("/support");
+    } else if (item === "모집") {
+      setBtn("모집");
+      router.push("/recruit");
     }
-  }
+  };
+
+  const SpaceHome = () => {
+    setBtn("");
+    router.push("/");
+  };
+
+  const SpaceMypage = () => {
+    setDrop(!drop);
+  };
+
+  const Login = (text: any) => {
+    if (text == "로그인") {
+      setAuth(!auth);
+    } else {
+      setAuth(!auth);
+      setDrop(!drop);
+    }
+  };
 
   return (
     <div>
       <div>
         <div className="navbar">
-          <div className="left">매니저브</div>
+          <div className="left" onClick={SpaceHome}>
+            매니저브
+          </div>
           <div className="center">
             <li
-              className={btn === '홍보글' ? 'selected' : ''}
-              onClick={() => select('홍보글')}
+              className={btn === "홍보글" ? "selected" : ""}
+              onClick={() => select("홍보글")}
             >
               <p>홍보글 둘러보기</p>
             </li>
             <li
-              className={btn === '지원' ? 'selected' : ''}
-              onClick={() => select('지원')}
+              className={btn === "지원" ? "selected" : ""}
+              onClick={() => select("지원")}
             >
               <p>조직 지원 관리</p>
             </li>
             <li
-              className={btn === '모집' ? 'selected' : ''}
-              onClick={() => select('모집')}
+              className={btn === "모집" ? "selected" : ""}
+              onClick={() => select("모집")}
             >
               <p>조직 모집 관리</p>
             </li>
           </div>
           <div className="right">
-            <img
-              width={20}
-              src="https://cdn-icons-png.flaticon.com/512/14/14660.png"
-            />
-            <div className="name">배현서</div>
+            {!auth ? (
+              <div className="login" onClick={() => Login("로그인")}>
+                <div className="text">로그인</div>
+              </div>
+            ) : (
+              <div style={{ display: "flex", width: "100px" }}>
+                <img
+                  width={20}
+                  src="https://cdn-icons-png.flaticon.com/512/14/14660.png"
+                />
+                <div className="name" onClick={SpaceMypage}>
+                  배현서
+                </div>
+                {drop ? (
+                  <div>
+                    <div className="drop-triangle"></div>
+                    <div className="drop">
+                      <div className="drop-container">
+                        <div
+                          className="text"
+                          style={{ textAlign: "center", lineHeight: "28px" }}
+                        >
+                          현서님, 좋은 조직 찾으시길 바랄게요 ^^{" "}
+                        </div>
+                        <div className="control">
+                          <div style={{ marginBottom: "10px" }}>설정</div>
+                          <div onClick={() => Login("로그아웃")}>로그아웃</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ) : (
+                  ""
+                )}
+              </div>
+            )}
           </div>
         </div>
         {children}
@@ -68,6 +127,7 @@ const Navbar = ({ children }: NavBarProps) => {
           font-size: 18px;
           line-height: 20px;
           color: #5c59f0;
+          cursor: pointer;
         }
 
         .center {
@@ -97,14 +157,65 @@ const Navbar = ({ children }: NavBarProps) => {
         .right {
           display: flex;
           margin-right: 30px;
+          cursor: pointer;
+          z-index: 1;
+        }
+        .right .login {
+          width: 100px;
+          height: 30px;
+
+          background: #44cecd;
+          border-radius: 20px;
+        }
+        .login .text {
+          color: white;
+          text-align: center;
+          margin-top: 9px;
+          font-size: 12px;
+          font-weight: 700;
         }
         .right .name {
           margin-left: 8px;
           color: #656565;
         }
+        .right .drop-triangle {
+          position: absolute;
+          z-index: 2;
+          border-bottom: 20px solid lightgray;
+          border-left: 20px solid transparent;
+          border-right: 20px solid transparent;
+          margin-right: 60px;
+          right: 0;
+          margin-top: 25px;
+          border-radius: 10px;
+        }
+        .right .drop {
+          position: absolute;
+          z-index: 3;
+          width: 177px;
+          height: 232px;
+          background-color: lightgray;
+          margin-top: 35px;
+          margin-right: 30px;
+          right: 0;
+          border-radius: 10px;
+          display: flex;
+        }
+        .drop-container {
+          width: 134px;
+          height: 165px;
+          margin: auto;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+        }
+        .drop-container .control div {
+          display: flex;
+          justify-content: center;
+        }
       `}</style>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
