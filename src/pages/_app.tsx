67728ba247c71e6d1bdc734b83d8be4d09f7cade
ballsona <ballsona0925@naver.com>
2023-05-15 23:43:57 +0900

    [feat] 글로벌 스타일 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,18 @@
-import globalStyles from '@/styles/global'
-import type { AppProps } from 'next/app'
-import Navbar from '@/components/Navbar'
-import '@/assets/fonts/font.css'
+import type { AppProps } from 'next/app';
+import Navbar from '@/components/Navbar';
+
+import globalStyles from '@/styles/global';
+import '@/assets/fonts/font.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Navbar>
-      <Component {...pageProps} />
+    <>
+      <Navbar>
+        <Component {...pageProps} />
+      </Navbar>
       <style jsx global>
         {globalStyles}
       </style>
-    </Navbar>
-  )
+    </>
+  );
 }
