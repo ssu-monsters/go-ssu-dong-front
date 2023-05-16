4ef33d34abcb444bcb49f5765f6a92063fbceb80
hoyy <110888511+hoyyChoi@users.noreply.github.com>
2023-05-16 18:20:18 +0900

    feat : 라우터 기능 추가 (#3)
    * [feat] 라우팅 페이지 제작
    
    * [feat] 네비게이션 바 작업
    
    * [fix] gitignore
    
    * [update] ingnore 파일 업데이트
    
    * [add] recoil 라이브러리 추가
    
    * [fix] 변수 네이밍 수정
    
    ---------
    
    Co-authored-by: ballsona0925 <ballsona0925@naver.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,21 @@
-import type { AppProps } from 'next/app';
-import Navbar from '@/components/Navbar';
+import globalStyles from "@/styles/global";
+import type { AppProps } from "next/app";
+import Navbar from "@/components/Navbar";
+import "@/assets/fonts/font.css";
+import { RecoilRoot } from "recoil";
 
 import globalStyles from '@/styles/global';
 import '@/assets/fonts/font.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <RecoilRoot>
       <Navbar>
         <Component {...pageProps} />
+        <style jsx global>
+          {globalStyles}
+        </style>
       </Navbar>
-      <style jsx global>
-        {globalStyles}
-      </style>
-    </>
+    </RecoilRoot>
   );
 }
