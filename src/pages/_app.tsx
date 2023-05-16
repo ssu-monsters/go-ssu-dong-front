62f420cd673be02bc7664596e2861d887366f649
hoyyChoi <hoyeon8165@gmail.com>
2023-05-16 16:08:08 +0900

    [add] recoil 라이브러리 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,18 @@
-import globalStyles from '@/styles/global'
-import type { AppProps } from 'next/app'
-import Navbar from '@/components/Navbar'
-import '@/assets/fonts/font.css'
+import globalStyles from "@/styles/global";
+import type { AppProps } from "next/app";
+import Navbar from "@/components/Navbar";
+import "@/assets/fonts/font.css";
+import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Navbar>
-      <Component {...pageProps} />
-      <style jsx global>
-        {globalStyles}
-      </style>
-    </Navbar>
-  )
+    <RecoilRoot>
+      <Navbar>
+        <Component {...pageProps} />
+        <style jsx global>
+          {globalStyles}
+        </style>
+      </Navbar>
+    </RecoilRoot>
+  );
 }
