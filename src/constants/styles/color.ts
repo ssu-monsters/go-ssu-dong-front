d5ba18a3bcdc84fef5b53d14251da1e328e10bb6
ballsona <ballsona0925@naver.com>
2023-05-29 20:27:46 +0900

    style : 배경 색상 추가 및 global style 수정

--- src/constants/styles/color.ts
+++ src/constants/styles/color.ts
@@ -1,23 +1,19 @@
-
-
 export const COLORS = {
-    primary: {
-      purple: '#5C59F0',
-    },
-    grayscale: {
-      white: '#FFFFFF',
-      gray50: '#ADADAD',
-      gray100: '#9E9E9E',
-      gray200: '#818181',
-      gray300: '#636363',
-      gray400: '#464646',
-      gray500: '#282828',
-      gray600: '#202020',
-      gray700: '#181818',
-      gray800: '#101010',
-      gray900: '#080808',
-    },
-
-  };
-
-
+  primary: {
+    purple: '#5C59F0',
+  },
+  grayscale: {
+    white: '#FFFFFF',
+    gray50: '#ADADAD',
+    gray100: '#9E9E9E',
+    gray200: '#818181',
+    gray300: '#636363',
+    gray400: '#464646',
+    gray500: '#282828',
+    gray600: '#202020',
+    gray700: '#181818',
+    gray800: '#101010',
+    gray900: '#080808',
+  },
+  background: '#FEFEFF',
+};
