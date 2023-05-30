8b1db09724f77d3a2d8f9724918fc2e2c27e9c7c
hoyyChoi <hoyeon8165@gmail.com>
2023-05-30 19:06:40 +0900

    [fix] 중괄호 추가

--- src/pages/recruit.tsx
+++ src/pages/recruit.tsx
@@ -1,14 +1,14 @@
-
 import PromotionWriting from '@/components/PromotionWrting';
 import RecruitTemplate from '@/components/RecruitTemplate';
 import React from 'react';
 
 const Recruit = () => {
-  return 
-  <div>
-    <PromotionWriting />;
-    <RecruitTemplate />;
-   </div>
+  return (
+    <div>
+      <PromotionWriting />;
+      <RecruitTemplate />;
+    </div>
+  );
 };
 
 export default Recruit;
