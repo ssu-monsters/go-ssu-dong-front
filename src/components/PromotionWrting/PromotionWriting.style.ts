5b3f827a9ab8129179ec4c8623f8e0ec7251d3de
ballsona <ballsona0925@naver.com>
2023-06-05 17:28:40 +0900

    style : remove font-family

--- src/components/PromotionWrting/PromotionWriting.style.ts
+++ src/components/PromotionWrting/PromotionWriting.style.ts
@@ -14,7 +14,6 @@ export default css`
   }
 
   .title {
-    font-family: 'NanumSquare Neo';
     font-style: normal;
     font-weight: 800;
     font-size: 40px;
@@ -24,7 +23,6 @@ export default css`
   }
 
   .container {
-    font-family: 'NanumSquare Neo';
     font-style: normal;
     font-weight: 700;
     font-size: 18px;
@@ -79,7 +77,7 @@ export default css`
   label {
     display: flex;
     align-items: center;
-    font-family: 'NanumSquare Neo';
+
     font-style: normal;
     font-weight: 400;
     font-size: 18px;
@@ -116,7 +114,6 @@ export default css`
   }
 
   .img-text {
-    font-family: 'NanumSquare Neo';
     font-style: normal;
     font-weight: 400;
     font-size: 18px;
