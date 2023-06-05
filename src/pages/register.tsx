3c4529d026e3fb98ad59bc9c7b3ea7c0b2710f8c
hoyyChoi <hoyeon8165@gmail.com>
2023-06-05 17:38:41 +0900

    feat : 회원가입 구현

---
+++ src/pages/register.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import RegisterBox from '@/components/RegisterBox';
+
+const register = () => {
+  return (
+    <div>
+      <RegisterBox />
+      <style jsx>{`
+        div {
+          font-family: 'NanumSquareNeo';
+          max-width: 1280px;
+          margin: auto;
+          margin-top: 66px;
+          padding-top: 10px;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          position: relative;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default register;
