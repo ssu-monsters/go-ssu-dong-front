78e9ef356948b22284205d9ac1b6a3002fc6d500
ballsona <ballsona0925@naver.com>
2023-06-05 17:30:47 +0900

    feat : add Apply and Recruit Sub menu Type

---
+++ src/constants/recruit.ts
@@ -0,0 +1,65 @@
+export const DOCUMENTONLY = [
+  {
+    step: 1,
+    title: '지원폼 생성하기',
+    content: '인재를 발굴하기 위해 지원자들에게 다양한 질문들을 물어보세요? 👋',
+    btn: '지원폼',
+  },
+  {
+    step: 2,
+    title: '홍보글 작성하기',
+    content: '매니저브 플랫폼에 홍보글을 올려보세요 ',
+    btn: '홍보글',
+  },
+  {
+    step: 3,
+    title: '지원자 관리하기',
+    content: '지원자를 한 번에 관리해보세요 ',
+    btn: '관리폼',
+  },
+  {
+    step: 4,
+    title: '최종 합격자 선별하기',
+    content: '최종 합격자들을 선별해보세요',
+    btn: '합격폼',
+  },
+];
+
+export const INTERVIEWONLY = [
+  {
+    step: 1,
+    title: '지원폼 생성하기',
+    content: '인재를 발굴하기 위해 지원자들에게 다양한 질문들을 물어보세요? 👋',
+    btn: '지원폼',
+  },
+  {
+    step: 2,
+    title: '홍보글 작성하기',
+    content: '매니저브 플랫폼에 홍보글을 올려보세요 ',
+    btn: '홍보글',
+  },
+  {
+    step: 3,
+    title: '지원자 관리하기',
+    content: '지원자를 한 번에 관리해보세요 ',
+    btn: '관리폼',
+  },
+  {
+    step: 4,
+    title: '1차 합격자 선별하기',
+    content: '1차 합격자들을 선별해보세요 ',
+    btn: '합격폼',
+  },
+  {
+    step: 5,
+    title: '면접 시작하기',
+    content: '면접을 위한 툴을 이용해보세요 ',
+    btn: '면접툴',
+  },
+  {
+    step: 6,
+    title: '최종 합격자 선별하기',
+    content: '최종 합격자들을 선별해보세요',
+    btn: '합격폼',
+  },
+];
