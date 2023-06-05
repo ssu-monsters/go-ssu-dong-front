fc77fc7bb0caac6aead343768e1dfb6afeb541ab
ballsona <ballsona0925@naver.com>
2023-06-05 17:29:41 +0900

    feat : refactor RecruitTemplate component

--- src/components/RecruitTemplate/RecruitTemplate.tsx
+++ src/components/RecruitTemplate/RecruitTemplate.tsx
@@ -1,70 +1,107 @@
-import React, { useEffect } from 'react';
-import Image from 'next/image';
-
-import Menu1Image from '@/assets/images/Megaphone.svg';
-import Menu2Image from '@/assets/images/Folder.svg';
-import Menu3Image from '@/assets/images/Chat.svg';
-
-import styles from './RecruitTemplate.style';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
-interface MyComponentProps {
-  setPageTransform: (newState: string) => void;
-}
+import RecruitProcessMenu from '../RecruitProcessMenu';
+import StepContainer from '../StepContainer';
 
-const RecruitTemplate = ({ setPageTransform }: MyComponentProps) => {
+import { DOCUMENTONLY, INTERVIEWONLY } from '@/constants/recruit';
+import { PageTransformType } from '@/constants/types/recruit';
+
+import styles from './RecruitTemplate.style';
+
+const RecruitTemplate = () => {
   const router = useRouter();
+  const [pageTransform, setPageTransform] = useState<PageTransformType | null>(
+    null,
+  );
 
-  const routingProcess = (params: string) => {
-    if (params === 'promotion') {
-      router.push(`/recruit/${params}`);
-    } else if (params === 'document') {
-      localStorage.setItem('pageTransform', 'document');
-      setPageTransform('document');
-    } else if (params === 'interview') {
-      localStorage.setItem('pageTransform', 'interview');
-      setPageTransform('interview');
+  const onClickProcessMenu = (menu: PageTransformType) => {
+    if (menu === 'promotion') {
+      router.push('/promotion/new');
     }
+    setPageTransform(menu);
+    localStorage.setItem('pageTransform', menu);
   };
+
+  useEffect(() => {
+    const pageTransformStoraged = localStorage.getItem('pageTransform');
+    if (pageTransformStoraged) {
+      setPageTransform(pageTransformStoraged as PageTransformType);
+    }
+  }, []);
+
+  const [stepActive, setStepActive] = useState(1);
+
+  const CreateForm = (doing: string) => {
+    // 생성하기 버튼 눌렀을때, 이벤트 발생로직
+    switch (doing) {
+      case '지원폼':
+        window.open('https://www.google.com/intl/ko_kr/forms/about/');
+        break;
+      case '홍보글':
+        router.push('/recruit/promotion'); // 이거 여기서 들어가는거랑 그냥 홍보글만 생성 구분해서 작성완료 눌렀을때, 여기서는 다시 돌아와야하고, 홍보글만 생성은 어떻게 해야하지?
+        break;
+      case '관리폼':
+        router.push('/recruit/2');
+        break;
+      case '합격폼':
+        router.push('/recruit/2');
+        break;
+    }
+    setStepActive(stepActive + 1);
+  };
+
+  function renderRecruitTemplate() {
+    if (pageTransform === 'document') {
+      return (
+        // 서류평가 지원
+        <div className="container">
+          <div className="title">
+            <span className="group-name">SSC</span> 의 리쿠르팅
+          </div>
+          <div>
+            {DOCUMENTONLY.map((item, idx) => {
+              return (
+                <div onClick={() => CreateForm(item?.btn)}>
+                  <StepContainer
+                    form={item}
+                    key={idx}
+                    stepActive={stepActive}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      );
+    } else if (pageTransform === 'interview') {
+      return (
+        // 서류평가 + 면접
+        <div className="container">
+          <div className="group-name">SSC 의 리쿠르팅</div>
+          <div>
+            {INTERVIEWONLY.map((item, idx) => {
+              return (
+                <div onClick={() => CreateForm(item?.btn)}>
+                  <StepContainer
+                    form={item}
+                    key={idx}
+                    stepActive={stepActive}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      );
+    } else {
+      return <RecruitProcessMenu onClickProcessMenu={onClickProcessMenu} />;
+    }
+  }
 
   return (
     <>
-      <div className="title">
-        <p>진행중인 리쿠르팅 프로세스가 없습니다.</p>
-        <p>저희와 함께 리쿠르팅을 시작해볼까요?</p>
-      </div>
-      <div className="menus-wrap">
-        <div className="menu-wrap" onClick={() => routingProcess('promotion')}>
-          <div className="image-container">
-            <Image src={Menu1Image} alt="only-promotion" />
-          </div>
-          <div className="intro-test">
-            저는 <span className="highlight">홍보글</span>만 <br /> 등록하고
-            싶어요!
-          </div>
-        </div>
-        <div className="menu-wrap" onClick={() => routingProcess('document')}>
-          <div className="image-container">
-            <Image src={Menu2Image} alt="resume-test" />
-          </div>
-          <div className="intro-test">
-            조직 리쿠르팅을 <br />
-            <span className="highlight">서류 평가</span>로 진행할게요!
-          </div>
-        </div>
-        <div
-          className="menu-wrap menu3"
-          onClick={() => routingProcess('interview')}
-        >
-          <div className="image-container">
-            <Image src={Menu3Image} alt="resume-interview-test" />
-          </div>
-          <div className="intro-test">
-            조직 리쿠르팅을 <br />
-            <span className="highlight">서류+면접 평가</span>로 진행할게요!
-          </div>
-        </div>
-      </div>
+      <div>{renderRecruitTemplate()}</div>
       <style jsx>{styles}</style>
     </>
   );
