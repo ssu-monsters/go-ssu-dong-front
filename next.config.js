94087e1be3188604a947eeb2c53a376a4555e364
ballsona <ballsona0925@naver.com>
2023-06-05 17:24:33 +0900

    feat : add axios and redirect configs

--- next.config.js
+++ next.config.js
@@ -5,6 +5,23 @@ const nextConfig = {
 
 module.exports = {
   ...nextConfig,
+  // menu redirect 관련 설정
+  async redirects() {
+    return [
+      {
+        source: '/apply',
+        destination: '/apply/profile',
+        permanent: false,
+      },
+      {
+        source: '/recruit',
+        destination: '/recruit/process',
+        permanent: true,
+      },
+    ];
+  },
+
+  // svgr/webpack 관련 설정
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
