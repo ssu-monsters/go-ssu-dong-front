d31fca2820b44261800bfc4374fe7e1276c3ea45
hoyyChoi <hoyeon8165@gmail.com>
2023-06-10 03:29:03 +0900

    [add] 더미데이터 추가

--- src/dummyData.ts
+++ src/dummyData.ts
@@ -218,3 +218,27 @@ export const organizationDummyData = [
   'qogustj@naver.com',
   '숭실대학교',
 ];
+
+export const promotionSliderDummyData = [
+  {
+    club: 'SSBC',
+    title: 'SSBC(중앙 농구 동아리)',
+    content: '자리 하나 남는다. 들어와라',
+    date: '🏀 4월 12일 마감 🏀',
+    img: '/SSBC.png',
+  },
+  {
+    club: 'SSC',
+    title: 'SSC(중앙 축구 동아리)',
+    content: '야, 너도 손흥민이 될 수 있어',
+    date: '⚽️ 4월 15일 마감 ⚽️',
+    img: '/SSC.png',
+  },
+  {
+    club: 'GDSC',
+    title: 'GDSC(Google Development Student Club)',
+    content: '개발과 Google 기술에 관심이 있다면?',
+    date: '💻 5월 1일 마감 💻',
+    img: '/GDSC.png',
+  },
+];
