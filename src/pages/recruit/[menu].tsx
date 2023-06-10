74e827d67bb80d8484e726fd39fadc0838120dcd
ballsona <ballsona0925@naver.com>
2023-06-10 22:55:08 +0900

    fix : fix build error

--- src/pages/recruit/[menu].tsx
+++ src/pages/recruit/[menu].tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SemiNavbar from '@/components/SemiNavbar';
 import UserInfo from '@/components/UserInfo';
 import RecruitTemplate from '@/components/RecruitTemplate';
